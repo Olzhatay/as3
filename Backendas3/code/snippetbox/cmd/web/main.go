f3328bc76eb9793f3ac651be9f0bf9be8f05c2b1
Olzhatay <96712442+Olzhatay@users.noreply.github.com>
2022-10-15 18:36:28 +0600

    AS3
    AS3

---
+++ Backendas3/code/snippetbox/cmd/web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	infoLog.Printf("running server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+
+}
+
+//перенаправить потоки из stdout и stderr в файлы на диске при запуске приложения
+//mv ui/html/pages/home.tmpl ui/html/pages/home.bak исскуственный internal error
+
+
